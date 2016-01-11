797b7ed30d9c19212eab41aef8999920dbf8b5d9
tmehta2442 <tmehta@post.harvard.edu>
2016-01-10 23:27:11 -0800

    some sprites

--- Sprite iOS/GameScene.swift
+++ Sprite iOS/GameScene.swift
@@ -9,32 +9,41 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var score : Int = 0
+    var jumpHeight : Double = 1.2
+    var playerName : String = "Ryan"
+    var isAlive : Bool = true
+    var isLevel1Completed : Bool = false
+    var isGamePaused : Bool = false
+    let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+    let sprite = SKSpriteNode(imageNamed:"Spaceship")
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+        printLabel()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
         
         for touch in touches {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
+            
+            sprite.xScale = 0.2
+            sprite.yScale = 0.2
             sprite.position = location
             
             let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
             
             sprite.runAction(SKAction.repeatActionForever(action))
             
+            score = score + 1
+            playerName = "Abid"
+            isAlive = false
+            
+            removePlane()            
+            changeMyLabelText()
             self.addChild(sprite)
         }
     }
@@ -42,4 +51,23 @@ class GameScene: SKScene {
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    func printLabel() {
+        
+        changeMyLabelText()
+        myLabel.fontSize = 35
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        self.addChild(myLabel)
+    }
+    
+    func changeMyLabelText() {
+        myLabel.text = "\(score) \(playerName) \(isAlive)"
+    }
+    
+    func removePlane() {
+        sprite.removeFromParent()
+    }
+    
 }
+
